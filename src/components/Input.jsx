244cbfd6943a64c0c48876c9b8dd5be0b9e821fc
Barış Kahraman <baris-kahra@hotmail.com>
2024-05-23 00:03:42 +0300

    İnputlar için Input componenti oluşturuldu ve props olarak değerler gönderildi.

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function Input() {
+export default function Input(props) {
+  const [inputValue , setInputValue] = useState('');
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  }
   return (
     <>
-      
+      <input
+          id={props.id}
+          className={props.className}
+          type={props.type}
+          value={inputValue}
+          onChange={handleChange}
+          placeholder={props.placeholder}
+          required
+      />
     </>
   )
 }
